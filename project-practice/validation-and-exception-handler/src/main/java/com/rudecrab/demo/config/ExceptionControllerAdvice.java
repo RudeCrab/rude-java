5b014448d1ebb44305ffa75bb3af2b387f4a07c2
RC <RudeCrab@foxmail.com>
2020-02-19 18:21:26 +0800

    添加全局异常处理

---
+++ project-practice/validation-and-exception-handler/src/main/java/com/rudecrab/demo/config/ExceptionControllerAdvice.java
@@ -0,0 +1,29 @@
+package com.rudecrab.demo.config;
+
+import com.rudecrab.demo.exception.APIException;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @author RudeCrab
+ * @description 全局异常处理
+ */
+@RestControllerAdvice
+public class ExceptionControllerAdvice {
+
+    @ExceptionHandler(APIException.class)
+    public String APIExceptionHandler(APIException e) {
+        return e.getMsg();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public String MethodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+        // 从异常对象中拿到ObjectError对象
+        ObjectError objectError = e.getBindingResult().getAllErrors().get(0);
+        // 然后提取错误提示信息进行返回
+        return objectError.getDefaultMessage();
+    }
+
+}
---
+++ project-practice/validation-and-exception-handler/src/main/java/com/rudecrab/demo/exception/APIException.java
@@ -0,0 +1,27 @@
+package com.rudecrab.demo.exception;
+
+import lombok.Getter;
+
+/**
+ * @author RudeCrab
+ * @description 自定义异常
+ */
+@Getter //只要getter方法，无需setter
+public class APIException extends RuntimeException {
+    private int code;
+    private String msg;
+
+    public APIException() {
+        this(1001, "接口错误");
+    }
+
+    public APIException(String msg) {
+        this(1001, msg);
+    }
+
+    public APIException(int code, String msg) {
+        super(msg);
+        this.code = code;
+        this.msg = msg;
+    }
+}
